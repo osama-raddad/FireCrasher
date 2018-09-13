1764e169bec8ae8e4a0c9d88fb99c2219a5e1fd3
AhmeDroid <ahmad.awad@arabiaweather.com>
2018-09-13 20:24:30 +0300

    Code Structure Cleanup

--- firecrasher/src/main/java/com/osama/firecrasher/FireCrasher.java
+++ firecrasher/src/main/java/com/osama/firecrasher/FireCrasher.java
@@ -9,52 +9,56 @@ import android.util.Log;
 
 
 public class FireCrasher {
-  public static void install(Application application) {
-    if (!FireLooper.isSafe()) {
-      CrashHandler crashHandler = new CrashHandler();
-      application.registerActivityLifecycleCallbacks(crashHandler.getLifecycleCallbacks());
-      FireLooper.install();
-      FireLooper.setUncaughtExceptionHandler(crashHandler);
-      Thread.setDefaultUncaughtExceptionHandler(crashHandler);
-    }
-  }
-
-  public static void install(Application application, CrashListener crashListener) {
-    if (!FireLooper.isSafe()) {
-      CrashHandler crashHandler = new CrashHandler();
-      crashHandler.setCrashListener(crashListener);
-      application.registerActivityLifecycleCallbacks(crashHandler.getLifecycleCallbacks());
-      FireLooper.install();
-      FireLooper.setUncaughtExceptionHandler(crashHandler);
-      Thread.setDefaultUncaughtExceptionHandler(crashHandler);
-    }
-  }
-
-  public static void install(Application application, CrashInterface crashListener) {
-    if (!FireLooper.isSafe()) {
-      CrashHandler crashHandler = new CrashHandler();
-      crashHandler.setCrashInterface(crashListener);
-      application.registerActivityLifecycleCallbacks(crashHandler.getLifecycleCallbacks());
-      FireLooper.install();
-      FireLooper.setUncaughtExceptionHandler(crashHandler);
-      Thread.setDefaultUncaughtExceptionHandler(crashHandler);
-    }
-  }
-
-  static void recover(Activity activity) {
-    if (activity != null) {
-      try {
-        ActivityInfo[] list = activity.getPackageManager()
-            .getPackageInfo(activity.getPackageName(), PackageManager.GET_ACTIVITIES).activities;
-        if (list.length == 1 && list[0].name.equals(activity.getClass().getName())) {
-          activity.finish();
-          activity.startActivity(new Intent(activity, activity.getClass()));
-        } else {
-          activity.onBackPressed();
+    public static void install(Application application) {
+        if (!FireLooper.isSafe()) {
+            CrashHandler crashHandler = new CrashHandler();
+            application.registerActivityLifecycleCallbacks(crashHandler.getLifecycleCallbacks());
+            FireLooper.install();
+            FireLooper.setUncaughtExceptionHandler(crashHandler);
+            Thread.setDefaultUncaughtExceptionHandler(crashHandler);
         }
-      } catch (PackageManager.NameNotFoundException e) {
-        Log.e("FireCrasher", e.getMessage());
-      }
     }
-  }
+
+    public static void install(Application application, CrashListener crashListener) {
+        if (!FireLooper.isSafe()) {
+            CrashHandler crashHandler = new CrashHandler();
+            crashHandler.setCrashListener(crashListener);
+            application.registerActivityLifecycleCallbacks(crashHandler.getLifecycleCallbacks());
+            FireLooper.install();
+            FireLooper.setUncaughtExceptionHandler(crashHandler);
+            Thread.setDefaultUncaughtExceptionHandler(crashHandler);
+        }
+    }
+
+    public static void install(Application application, CrashInterface crashListener) {
+        if (!FireLooper.isSafe()) {
+            CrashHandler crashHandler = new CrashHandler();
+            crashHandler.setCrashInterface(crashListener);
+            application.registerActivityLifecycleCallbacks(crashHandler.getLifecycleCallbacks());
+            FireLooper.install();
+            FireLooper.setUncaughtExceptionHandler(crashHandler);
+            Thread.setDefaultUncaughtExceptionHandler(crashHandler);
+        }
+    }
+
+    static void recover(Activity activity) {
+        if (activity != null) {
+            try {
+                ActivityInfo[] list = activity.getPackageManager()
+                        .getPackageInfo(activity.getPackageName(), PackageManager.GET_ACTIVITIES).activities;
+                if (list.length == 1 && list[0].name.equals(activity.getClass().getName())) {
+                    try {
+                        activity.startActivity(new Intent(activity, activity.getClass()));
+                    } catch (Exception e) {
+                        // e.printStackTrace();
+                    }
+                    activity.finish();
+                } else {
+                    activity.onBackPressed();
+                }
+            } catch (PackageManager.NameNotFoundException e) {
+                Log.e("FireCrasher", e.getMessage());
+            }
+        }
+    }
 }
